f98628b1aa08599d97ead9833bf08d0d46fd4983
Tuan Anh <24100351@st.phenikaa-uni.edu.vn>
2024-12-10 16:41:10 +0700

    use dotenv

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config();
 mongoose.connect("mongodb://localhost:27017/products");
 const router = require("./routes/client/index.router");
 const pug = require("pug");
 const app = express();
-const port = 3001;
+const port = process.env.Port;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 router(app);
 
 app.listen(port, () => {
-  console.log("Đã chạy thành công với cổng ");
+  console.log(`Đã chạy thành công với cổng : ${port} `);
 });
