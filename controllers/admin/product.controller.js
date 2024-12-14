b0a958bd59b6c78f2822aab5d93e1e945d4151a9
Tuan Anh <24100351@st.phenikaa-uni.edu.vn>
2024-12-14 15:01:36 +0700

    update filter

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -3,11 +3,47 @@
 const products = require("../../models/sanpham");
 
 module.exports = async (req, res) => {
-  const sanphamadmin = await products.find({
+  const button = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: "",
+    },
+    {
+      name: "Hoạt động",
+      status: "active",
+      class: "",
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: "",
+    },
+  ];
+
+  if (req.query.status) {
+    const index = button.findIndex((item) => {
+      return item.status == req.query.status;
+    });
+    button[index].class = "active";
+  } else {
+    const index = button.findIndex((item) => {
+      return item.status == "";
+    });
+    button[index].class = "active";
+  }
+
+  let find = {
     delected: "false",
-  });
+  };
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+
+  const sanphamadmin = await products.find(find);
   res.render("admin/pages/products/index.pug", {
     title: "Trang San Pham",
     sanphamadmin: sanphamadmin,
+    button: button,
   });
 };
---
+++ public/admin/js/script.js
@@ -0,0 +1,15 @@
+// button status
+
+const buttonstatus = document.querySelectorAll("[button-status]");
+let url = new URL(window.location.href);
+buttonstatus.forEach((button) => {
+  button.addEventListener("click", () => {
+    const status = button.getAttribute("button-status");
+    if (status) {
+      url.searchParams.set("status", status);
+    } else {
+      url.searchParams.delete("status");
+    }
+    window.location.href = url.href;
+  });
+});
