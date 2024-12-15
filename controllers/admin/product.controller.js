38ed87b7bb7f25600ff0075a9a14eef0386b90c5
Tuan Anh <24100351@st.phenikaa-uni.edu.vn>
2024-12-15 14:31:08 +0700

    update search 1.2

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -43,7 +43,8 @@ module.exports = async (req, res) => {
   let keyword = "";
   if (req.query.keyword) {
     keyword = req.query.keyword;
-    find.title = keyword;
+    const regex = new RegExp(keyword, "i");
+    find.title = regex;
   }
 
   const sanphamadmin = await products.find(find);
